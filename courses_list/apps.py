865a62bb6776c3f01f0cac2be61803a3e25ecd21
Muhammad Faraz  Maqsood <faraz.maqsood@A006-00763.local>
2023-04-10 14:09:54 +0500

    changes

--- courses_list/apps.py
+++ courses_list/apps.py
@@ -3,9 +3,9 @@ courses_list Django application initialization.
 """
 
 from django.apps import AppConfig
-from edx_django_utils.plugins.constants import (
-    PluginURLs
-)
+# from edx_django_utils.plugins.constants import (
+#     PluginURLs
+# )
 
 
 class CoursesListConfig(AppConfig):
@@ -17,25 +17,25 @@ class CoursesListConfig(AppConfig):
     plugin_app = {
 
         # Configuration setting for Plugin URLs for this app.
-        PluginURLs.CONFIG: {
+        "url_config": {
 
             # Configure the Plugin URLs for each project type, as needed.
             "lms.djangoapp": {
 
                 # The namespace to provide to django's urls.include.
-                PluginURLs.NAMESPACE: 'courses_list',
+                'namespace': 'courses_list',
 
                 # The application namespace to provide to django's urls.include.
                 # Optional; Defaults to None.
-                PluginURLs.APP_NAME: 'courses_list',
+                # PluginURLs.APP_NAME: 'courses_list',
 
                 # The regex to provide to django's urls.url.
                 # Optional; Defaults to r''.
-                PluginURLs.REGEX: r'api/courses_list',
+                'regex': r'api/courses_list',
 
                 # The python path (relative to this app) to the URLs module to be plugged into the project.
                 # Optional; Defaults to 'urls'.
-                PluginURLs.RELATIVE_PATH: 'urls',
+                'relative_path': 'urls',
             }
         },
    }
